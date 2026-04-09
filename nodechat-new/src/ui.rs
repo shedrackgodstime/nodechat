dd7c59983c5d3b8266304a2f3321dc649fa2fcdb
ultimatekristency <ultimatekristency@gmail.com>
2026-04-10 00:29:16 +0100

    first commit

--- nodechat-new/src/ui.rs
+++ nodechat-new/src/ui.rs
@@ -17,6 +17,21 @@ pub fn run_app() -> anyhow::Result<()> {
         }
     });
 
+    let handle_name = app.as_weak();
+    app.on_confirm_name_change(move |name| {
+        if let Some(ui) = handle_name.upgrade() {
+            ui.set_display_name(name);
+        }
+    });
+
+    let handle_pass = app.as_weak();
+    app.on_submit_password(move |_cur, new_pass| {
+        if let Some(ui) = handle_pass.upgrade() {
+            ui.set_has_password(!new_pass.is_empty());
+        }
+    });
+
+
     app.run().context("failed to run Slint window")?;
     Ok(())
 }
